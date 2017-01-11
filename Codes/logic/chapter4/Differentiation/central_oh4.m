9e48269c8d40534fcc10d6c6fd09b7f505a6c6ec
BlurM <mohebzadeh.mohsen@gmail.com>
2017-01-12 00:45:13 +0330

    chap 4 : Central Differencing O(h^2) & O(h^4) steps added, minor modifications

--- Codes/logic/chapter4/Differentiation/central_oh4.m
+++ Codes/logic/chapter4/Differentiation/central_oh4.m
@@ -3,25 +3,61 @@ function [steps, res] = central_oh4(F_str, pt, deg, h, FPD)
 %   Detailed explanation goes here
 
 digits(FPD);
-steps = cell(3, 1);
+steps = cell(6, 1);
+
+fppp = vpa(eval(subs(F_str, pt-3*h)));
+fpp = vpa(eval(subs(F_str, pt-2*h)));
+fp = vpa(eval(subs(F_str, pt-h)));
+f = vpa(eval(subs(F_str, pt)));
+fn = vpa(eval(subs(F_str, pt+h)));
+fnn = vpa(eval(subs(F_str, pt+2*h)));
+fnnn = vpa(eval(subs(F_str, pt+3*h)));
+
+steps{1} = 'The best method (with the lowest error) here';
+steps{2} = 'is the Central Differencing with O(h^4) error :';
+steps{3} = '';
 
 switch deg
     case 1
-        res = vpa((-eval(subs(F_str, pt+2*h))+8*eval(subs(F_str, pt+h))-...
-            8*eval(subs(F_str, pt-h))+eval(subs(F_str, pt-2*h)))/(12*h));
+        res = vpa((-fnn+8*fn-8*fp+fpp)/(12*h));
+        
+        steps{4} = ['f''_i = (-f_(i+2) + 8*f_(i+1) - 8*f_(i-1) + f_(i-2))', ...
+            ' / (12*h)'];
+        steps{5} = ['      = (-(', char(fnn), ') + 8*', char(fn), ' - 8*', ...
+            char(fp), ' + ', char(fpp), ') / ', char(vpa(12*h))];
+        steps{6} = ['      = ', char(res)];
+        
     case 2
-        res = vpa((-eval(subs(F_str, pt+2*h))+16*eval(subs(F_str, pt+h))-...
-            30*eval(subs(F_str, pt))+16*eval(subs(F_str, pt-h))-...
-            eval(subs(F_str, pt-2*h)))/(12*h^2));
+        res = vpa((-fnn+16*fn-30*f+16*fp-fpp)/(12*h^2));
+        
+        steps{4} = ['f''''_i = (-f_(i+2) + 16*f_(i+1) - 30*f_i + ' ...
+            '16*f_(i-1) - f_(i-2)) / (12*h^2)'];
+        steps{5} = ['      = (-(', char(fnn), ') + 16*', char(fn), ...
+            ' - 30*', char(f), ' + 16*', char(fp), '- (', char(fpp), ...
+            ') / ', char(vpa(12*h^2))];
+        steps{6} = ['      = ', char(res)];
+        
     case 3
-        res = vpa((-eval(subs(F_str, pt+3*h))+8*eval(subs(F_str, pt+2*h))-...
-            13*eval(subs(F_str, pt+h))+13*eval(subs(F_str, pt-h))-...
-            8*eval(subs(F_str, pt-2*h)))/(8*h^3));
+        res = vpa((-fnnn+8*fnn-13*fn+13*fp-8*fpp+fppp)/(8*h^3));
+    
+        steps{4} = ['f_3rd_i = (-f_(i+3) + 8*f_(i+2) - 13*f_(i+1) + ' ...
+            '13*f_(i-1) - 8*f_(i-2) + f_(i-3)) / (8*h^3)'];
+        steps{5} = ['      = (-(', char(fnnn), ') + 8*', char(fnn), ...
+            ' - 13*', char(fn), ' + 13*', char(fp), '- 8*', char(fpp), ...
+            ' + (', char(fppp), ')) / ', char(vpa(8*h^3))];
+        steps{6} = ['      = ', char(res)];
+    
     case 4
-        res = vpa((-eval(subs(F_str, pt+3*h))+12*eval(subs(F_str, pt+2*h))-...
-            39*eval(subs(F_str, pt+h))+56*eval(subs(F_str, pt))-...
-            39*eval(subs(F_str, pt-h))+...
-            12*eval(subs(F_str, pt-2*h)))/(6*h^4));
+        res = vpa((-fnnn+12*fnn-39*fn+56*f-39*fp+12*fpp-fppp)/(6*h^4));
+        
+        steps{4} = ['f_4th_i = (-f_(i+3) + 12*f_(i+2) - 39*f_(i+1) + ' ...
+            '56*f_i - 39*f_(i-1) + 12*f_(i-2) - f_(i-3)) / (6*h^4)'];
+        steps{5} = ['      = (-(', char(fnnn), ') + 12*', char(fnn), ...
+            ' - 39*', char(fn), ' + 56*', char(f), '- 39*', char(fp), ...
+            ' + 12*', char(fpp), ' - (', char(fppp), ')) / ', ...
+            char(vpa(6*h^4))];
+        steps{6} = ['      = ', char(res)];
+    
 end
 
 end
--- Codes/logic/chapter4/Differentiation/threepoint.m
+++ Codes/logic/chapter4/Differentiation/threepoint.m
@@ -3,7 +3,7 @@ function [steps, res] = threepoint(F_str, pt, h, FPD)
 %   Detailed explanation goes here
 
 digits(FPD);
-steps = cell(3, 1);
+steps = cell(6, 1);
 
 f = vpa(eval(subs(F_str, pt)));
 fnxt = vpa(eval(subs(F_str, pt+h)));
@@ -13,8 +13,8 @@ res = vpa((-3*f+4*fnxt-fnxtnxt)/(2*h));
 
 steps{1} = 'The best method (with the lowest error) here';
 steps{2} = 'is the Threepoint method :';
-steps{4} = '';
-steps{5} = 'f''_i = (-3f_i + 4f_(i+1) - f_(i+2)) / 2h';
+steps{3} = '';
+steps{4} = 'f''_i = (-3f_i + 4f_(i+1) - f_(i+2)) / 2h';
 steps{5} = ['      = (-3*', char(f), ' + 4*', char(fnxt), ' - ', ...
     char(fnxtnxt), ') / ', char(vpa(2*h))];
 steps{6} = ['      = ', char(res)];
